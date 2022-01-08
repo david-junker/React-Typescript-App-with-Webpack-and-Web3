05434df3f5739e0f125b5bbf66bb4902b497760d
David Junker <90758281+david-junker@users.noreply.github.com>
2022-01-08 14:43:33 +0100

    Delete src/secondPage directory

--- src/secondPage/SecondPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './SecondPage.css';
-import { Link } from 'react-router-dom';
-
-const SecondPage: React.FC = () => {
-  return <div className='SecondPage'>Second Page</div>;
-};
-
-export default SecondPage;
