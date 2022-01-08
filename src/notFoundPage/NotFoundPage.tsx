4da23262ceafaf88df3e2ecc2630768527cd7969
David Junker <90758281+david-junker@users.noreply.github.com>
2022-01-08 14:43:25 +0100

    Delete src/notFoundPage directory

--- src/notFoundPage/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './NotFoundPage.css';
-import { Link } from 'react-router-dom';
-
-const NotFoundPage: React.FC = () => {
-  return <div className='NotFoundPage'>404 Page not found</div>;
-};
-
-export default NotFoundPage;
